06ff44cfe7030dd6f313ae07eeb407deb5948d6b
Jeremias Erbs <jerry@badabam.de>
2022-01-31 14:59:59 +0100

    Initial commit

---
+++ greet.js
@@ -0,0 +1,4 @@
+// pure function, has no side effect
+export function greet(name) {
+  return 'Hello ' + name + '!';
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+import { greet } from './greet.js';
+
+// "impure", has side effects
+// function greet() {
+//   console.log('Hello world!');
+// }
+
+console.log(greet('Jane Doe'));
