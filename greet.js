4e665938f95d1cf0e5273bbce1fab6bd3987b394
Jeremias Erbs <jerry@badabam.de>
2022-01-31 16:34:14 +0100

    Add tests for greet

--- greet.js
+++ greet.js
@@ -1,4 +1,7 @@
 // pure function, has no side effect
-export function greet(name) {
-  return 'Hello ' + name + '!';
+export function greet(name = 'stranger') {
+  if (name === 'Mareike' || name === 'Paul' || name === 'Jerry') {
+    return 'Hello coach!';
+  }
+  return `Hello ${name}!`;
 }
